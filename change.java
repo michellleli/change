b4a8a0bc974ee546b3a6496b5f881815feef2b35
Michelle Li <michelleli@Michelles-MacBook-Air-2.local>
2020-09-02 13:57:12 -0700

    Initial hello world

---
+++ change.java
@@ -0,0 +1,11 @@
+public class change 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello world.");
+    }
+}
+
+//public int NumberOfWaysToMakeChange(int cents)
+//{
+//}
